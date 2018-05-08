f615778683410c690c660352491dce945e2a7ee8
barak menachem <barakmen@post.bgu.ac.il>
2018-05-08 18:55:36 +0300

    finish to work on the release files - ready to production

--- Release/bump-version.sh
+++ Release/bump-version.sh
@@ -2,3 +2,6 @@
 #of curse, this is *******NOT********* a new feature, its only last changes for the release.
 
 echo "Files modified successfully, version bumped to $1." > VERSION
+
+
+git log > CHANGES.log
